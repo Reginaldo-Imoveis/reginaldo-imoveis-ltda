f2c9e95c9c46a9f6d59737db6754860998cdc804
theocomparetti <theocomparetti@users.noreply.github.com>
2026-05-29 16:19:17 +0000

    deploy: 18d85639a8d040577eff8770d4ca7ed4c1851d0d

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'ri-shell-v7';
+const CACHE_NAME = 'ri-shell-v8';
 const SHELL_ASSETS = [
     '/',
-    '/css/style.css?v=16',
+    '/css/style.css?v=17',
     '/js/main.js?v=17',
     '/js/tracker.js',
     '/manifest.json'
